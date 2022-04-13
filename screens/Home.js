28e5f72cdc08137af6e09233a09a15f9e6be4e2d
md-amir-gauhar <gauharamir15@gmail.com>
2022-04-14 01:21:53 +0530

    added statusbar

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,23 @@
-import { Text } from "react-native"
+import {useState} from 'react'
+import { Text, SafeAreaView, FlatList, View } from "react-native"
+
+import {COLORS, NFTData} from '../constants'
+import {NFTCard, HomeHeader, FocusedStatusBar} from '../components'
 
 const Home = () => {
   return (
-    <Text>Home</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <FocusedStatusBar background={COLORS.primary} />
+
+      <View style={{flex: 1}}>
+        <View style={{zIndex: 0}}>
+          <FlatList
+            data={NFTData}
+            renderItem={({item}) => <Text>{item.name}</Text>}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
