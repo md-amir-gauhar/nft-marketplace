0d4e21d920cecd8269d07473d6a59a7abbf30485
md-amir-gauhar <gauharamir15@gmail.com>
2022-04-14 01:59:49 +0530

    feat: added Cards

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
 import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton, RectButton } from "./Button";
 
-export {HomeHeader, NFTCard, FocusedStatusBar}
+export {HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton}
